f110e0fa1a9223c5df5e1c92bda47753373eaa09
jiachen <jiachen@winlab.rutgers.edu>
2018-10-11 10:10:49 -0600

    Added files for large-scale fib eval.

--- tracegenerator/main.cpp
+++ tracegenerator/main.cpp
@@ -64,9 +64,13 @@ int main() {
 //    outputFib(names, "ben-client-1-B512.fib", 0x10, 0x10, rand);
 //    outputFib(names, "ben-router-B512.fib", 0x11, 0x16, rand);
 //    outputGNRS(names, "ben-client-1-B512.gnrs");
-    generateNamespace(0, 13, 2, 2, 8, 4, "/", names, rand);
-    outputFib(names, "ben-client-1-B16383.fib", 0x10, 0x10, rand);
-    outputFib(names, "ben-router-B16383.fib", 0x11, 0x16, rand);
-    outputGNRS(names, "ben-client-1-B16383.gnrs");
+//    generateNamespace(0, 13, 2, 2, 8, 4, "/", names, rand);
+//    outputFib(names, "ben-client-1-B16383.fib", 0x10, 0x10, rand);
+//    outputFib(names, "ben-router-B16383.fib", 0x11, 0x16, rand);
+//    outputGNRS(names, "ben-client-1-B16383.gnrs");
+    generateNamespace(0, 13, 2, 2, 15, 25, "/", names, rand);
+    outputFib(names, "ben-client-1-BL16383.fib", 0x10, 0x10, rand);
+    outputFib(names, "ben-router-BL16383.fib", 0x11, 0x16, rand);
+    outputGNRS(names, "ben-client-1-BL16383.gnrs");
     return 0;
 }
